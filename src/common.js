175a93affeead0050165c76292cb593f2c801241
Ed <barp.edoardo@gmail.com>
2022-11-25 15:21:08 +0100

    Improving boilerplate
    (cherry picked from commit 2744d21ecb8a53125b85bc86f24040f0b034bf49)

---
+++ src/common.js
@@ -0,0 +1,23 @@
+import neo4j from "neo4j-driver";
+import { NEO4J_PASSWORD, NEO4J_USER, NEO4J_URI } from "./env.js"
+import { typeDefs } from './graphql-schema.js'
+import pkg from '@neo4j/graphql-ogm';
+const { OGM } = pkg;
+
+
+
+/*
+ * Create a Neo4j driver instance to connect to the database.
+ */
+export const driver = neo4j.driver(
+    NEO4J_URI,
+    neo4j.auth.basic(
+        NEO4J_USER,
+        NEO4J_PASSWORD
+    )
+);
+
+
+export const ogm = new OGM({ typeDefs, driver });
+
+
--- src/index.js
+++ src/index.js
@@ -5,21 +5,11 @@ import express from 'express'
 import { Neo4jGraphQL } from '@neo4j/graphql'
 import { resolvers } from './resolves/definition.js'
 import { GRAPHQL_HOST, GRAPHQL_PORT, GRAPHQL_PATH, NEO4J_PASSWORD, NEO4J_USER, NEO4J_URI } from "./env.js"
-import neo4j from "neo4j-driver";
+import { ogm, driver } from './common.js' 
 
 
 const app = express()
 
-/*
- * Create a Neo4j driver instance to connect to the database.
- */
-export const driver = neo4j.driver(
-    NEO4J_URI,
-    neo4j.auth.basic(
-        NEO4J_USER,
-        NEO4J_PASSWORD
-    )
-);
 
 /*
  * Create an executable GraphQL schema object from GraphQL type definitions
@@ -33,40 +23,44 @@ const neoSchema = new Neo4jGraphQL({
     driver,
 })
 
-/*
- * Create a new ApolloServer instance, serving the GraphQL schema
- * created using makeAugmentedSchema above and injecting the Neo4j driver
- * instance into the context object, so it is available in the
- * generated resolvers to connect to the database.
- */
-const server = new ApolloServer({
-    schema: await neoSchema.getSchema(),
-    context: ({ req }) => ({ req, driver }),
-    introspection: true,
-    playground: true,
-    plugins: [
-        ApolloServerPluginLandingPageLocalDefault()
-    ]
-})
 
-server.start().then(() => {
-    server.applyMiddleware({ app, path: GRAPHQL_PATH });
-    app.listen({ host: GRAPHQL_HOST, port: GRAPHQL_PORT, path: GRAPHQL_PATH }, async () => {
-        let attempt = 0;
-        while(attempt < 3){
-          try {
-            console.log("Asserting constraints and indexes")
-            await neoSchema.assertIndexesAndConstraints({ options: { create: true }});
-            break;
-          }catch(e){
-            // It is most likely is an error due to the database not being up and running yet. 
-            // We still print the exception string to make sure it's not something more
-            attempt += 1;
-            console.log(`${e}`);
-            console.log(`Caught error while checking database constraints, retrying in 30 seconds [${attempt}/3]`);
-            await new Promise(r => setTimeout(r, 30000));
+Promise.all([neoSchema.getSchema(), ogm.init()]).then(([schema]) => {
+
+  /*
+  * Create a new ApolloServer instance, serving the GraphQL schema
+  * created using makeAugmentedSchema above and injecting the Neo4j driver
+  * instance into the context object, so it is available in the
+  * generated resolvers to connect to the database.
+  */
+  const server = new ApolloServer({
+      schema: schema,
+      context: ({ req }) => ({ req, driver }),
+      introspection: true,
+      playground: true,
+      plugins: [
+          ApolloServerPluginLandingPageLocalDefault()
+      ]
+  })
+
+  server.start().then(() => {
+      server.applyMiddleware({ app, path: GRAPHQL_PATH });
+      app.listen({ host: GRAPHQL_HOST, port: GRAPHQL_PORT, path: GRAPHQL_PATH }, async () => {
+          let attempt = 0;
+          while(attempt < 3){
+            try {
+              console.log("Asserting constraints and indexes")
+              await neoSchema.assertIndexesAndConstraints({ options: { create: true }});
+              break;
+            }catch(e){
+              // It is most likely is an error due to the database not being up and running yet. 
+              // We still print the exception string to make sure it's not something more
+              attempt += 1;
+              console.log(`${e}`);
+              console.log(`Caught error while checking database constraints, retrying in 30 seconds [${attempt}/3]`);
+              await new Promise(r => setTimeout(r, 30000));
+            }
           }
-        }
-        console.log(`GraphQL server ready at http://${GRAPHQL_HOST}:${GRAPHQL_PORT}${GRAPHQL_PATH}`)
-      })
+          console.log(`GraphQL server ready at http://${GRAPHQL_HOST}:${GRAPHQL_PORT}${GRAPHQL_PATH}`)
+        })
+  });
 });
